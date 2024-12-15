7f5d87bb7c2ac6045a0130ef878fe90a16bf5db5
Shyam Patel <shyamp124@gmail.com>
2024-12-15 02:10:42 -0500

    Finished day3

---
+++ day3/part1/part1.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"unicode"
+)
+
+func main() {
+	data, err := os.ReadFile("../day3.txt")
+	if err != nil {
+		fmt.Println("Failed to parse file")
+		return
+	}
+	fileContent := string(data)
+	contentLength := len(fileContent)
+	fileContent = fileContent + "######################"
+
+	i := 0
+	total := 0
+	for i < contentLength {
+		mulCheck := fileContent[i : i+3]
+		if mulCheck == "mul" {
+			i += 3
+			if fileContent[i] == '(' {
+				i++
+				digit1 := 0
+				j := 0
+				flag := false
+				for j < 3 && fileContent[i] != ',' {
+					if unicode.IsDigit(rune(fileContent[i])) {
+						digit1 = digit1*10 + int(fileContent[i]-'0')
+					} else {
+						flag = true
+						break
+					}
+					i++
+					j++
+				}
+				i++
+				if flag {
+					continue
+				}
+				digit2 := 0
+				j = 0
+				flag = false
+				for j < 3 && fileContent[i] != ')' {
+					if unicode.IsDigit(rune(fileContent[i])) {
+						digit2 = digit2*10 + int(fileContent[i]-'0')
+					} else {
+						flag = true
+						break
+					}
+					i++
+					j++
+				}
+				if !flag && fileContent[i] == ')' {
+					total += digit1 * digit2
+				}
+			}
+		} else {
+			i++
+		}
+	}
+	fmt.Println(total)
+}
